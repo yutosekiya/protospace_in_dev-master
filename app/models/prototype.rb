c94f93e25ade9b782eb5b30d8d14186197331ce3
Th0rt <yamaxanado@gmail.com>
2018-06-15 14:08:35 +0900

    [update] model setting

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,10 +1,12 @@
 class Prototype < ActiveRecord::Base
+  MAX_TAG_LENGTH = 3
+
   belongs_to :user
   has_many :captured_images, dependent: :destroy
   has_many :prototype_tags, dependent: :destroy
   has_many :tags, through: :prototype_tags
 
-  accepts_nested_attributes_for :tags
+  accepts_nested_attributes_for :tags, reject_if: :reject_empty_content, allow_destroy: true
   accepts_nested_attributes_for :captured_images, reject_if: :reject_empty_content
 
   validates :title,
@@ -12,8 +14,18 @@ class Prototype < ActiveRecord::Base
             :concept,
             presence: true
 
+  validates :tags, length: { maximum: MAX_TAG_LENGTH }
+
+  before_save :delete_blank_tag_relation
+
   def reject_empty_content(attributed)
     attributed['content'].blank?
+  end
+
+  # Tagのcontentsが""の場合、Relationを削除
+  def delete_blank_tag_relation
+    tags.each{ |tag| tag.mark_for_destruction if tag.content.blank? }
+    true
   end
 
   def set_main_thumbnail
