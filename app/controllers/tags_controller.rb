d7ff19eb140076bac708cedde9fa888993c2f590
関谷佑斗 <yhk3165t@gmail.com>
2018-06-15 15:40:36 +0900

    tags_controller にindex、showアクションを設定

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,12 @@
 class TagsController < ApplicationController
   def index
+    @tags = Tag.all
   end
 
   def create
   end
 
+  def show
+    @prototypes = Tag.find(params[:id]).prototypes.page(params[:page]).per(5)
+  end
 end
