5bd45ea2879dc99b6b7df715c2ca4ee9bb2a16db
Th0rt <yamaxanado@gmail.com>
2018-06-22 21:22:06 +0900

    [update] ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes
+  resources :prototypes do 
+    resources :likes, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update]
   resources :tags, only: [:index,:show]
 end
